097ce73473e9c0d0a0df443d5531637cbbe184a2
Seongjin Choi <Seongjin Choi>
2015-05-22 12:56:34 -0400

    Updated
    Updated Inference project.

--- inf_prj.R
+++ inf_prj.R
@@ -1,56 +1,26 @@
-# Test
-#rexp_n40_rate0_2 = rexp(n = 40, rate = 0.2)
-#hist(rexp_n40_rate0_2)
-#var(rexp_n40_rate0_2)
-
-#set.seed(123) ## I set this in order to make my simulation results reproducible. 
-#n <- 1000
-#means <- cumsum(rnorm(n))/(1:n)
-#hist(means)
-
-
-
-## Number of simulation 
+### 1000 repetitions of runif
 set.seed(123)
-mexps100 = NULL
-for (i in 1 : 100) {
-    mexps100 = c(mexps100, mean(rexp(n = 40, rate = 0.2)))
-}
-hist(mexps100)
+hist(runif(n = 1000))
 
-
-mexps200 = NULL
-for (i in 1 : 200) {
-    mexps200 = c(mexps200, mean(rexp(n = 40, rate = 0.2)))
-}
-hist(mexps200)
-
-
-mexps500 = NULL
-for (i in 1 : 500) {
-    mexps500 = c(mexps500, mean(rexp(n = 40, rate = 0.2)))
-}
-hist(mexps500)
-
-
-mexps1k = NULL
+## 1000 averages of 40 averages of runif
+mns = NULL
 for (i in 1 : 1000) {
-    mexps1k = c(mexps1k, mean(rexp(n = 40, rate = 0.2)))
+      mns = c(mns, mean(runif(40)))
+}      
+hist(mns)
+
+
+
+## Project 
+## 1000 of repetitions of exponential distribusion with rate = 0.2 
+set.seed(123)
+rexp.1000 <- rexp(n = 1000, rate = 0.2)
+hist(rexp.1000)
+
+##  
+set.seed(123) ## Purpose of this line to make this simulation reproducible. 
+mexps <- NULL
+for (i in 1 : 1000) {
+    mexps <- c(mexps, mean(rexp(n = 40, rate = 0.2)))
 }
-hist(mexps1k)
-
-
-mexps2k = NULL
-for (i in 1 : 2000) {
-    mexps2k = c(mexps2k, mean(rexp(n = 40, rate = 0.2)))
-}
-hist(mexps2k)
-
-
-mexps5k = NULL
-for (i in 1 : 5000) {
-    mexps5k = c(mexps5k, mean(rexp(n = 40, rate = 0.2)))
-}
-hist(mexps5k)
-
-
+hist(mexps)
