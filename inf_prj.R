3b9383490f28da4d90220dfe601127dc3f17041c
Seongjin Choi <Seongjin Choi>
2015-05-23 12:50:42 -0400

    Updated
    Updated

--- inf_prj.R
+++ inf_prj.R
@@ -1,6 +1,5 @@
 ### 1000 collections of runifs
-mn <- NULL
-mn <- mean((runif(n = 1000)))
+hist(runif(n = 40))
 hist(runif(n = 1000))
 
 ## 1000 collections of averages of 40 runifs
@@ -13,10 +12,12 @@ hist(mns)
 
 
 ## Project ################################################################################
-## 1000 collections of random exponentials (with rate = 2)
+## 40 random exponentials (with rate = 2)
+hist(rexp(n = 40, rate = 0.2))
+## 1000 random exponentials (with rate = 2)
 hist(rexp(n = 1000, rate = 0.2))
 
-## 1000 collections of averages of 40 exponentials 
+## 1000 collections of averages of 40 random exponentials 
 mexps <- NULL
 for (i in 1 : 1000) {
     mexps <- c(mexps, mean(rexp(n = 40, rate = 0.2))) 
